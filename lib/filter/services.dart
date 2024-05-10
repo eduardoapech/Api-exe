a8d40716c06590e8631612e3fbb9bbe3a46e903c
Eduardo Augusto Pech <eduardo.pech@abase.local>
2024-05-10 09:18:21 -0300

    s

--- lib/filter/services.dart
+++ lib/filter/services.dart
@@ -1,30 +1,19 @@
 import 'package:api_dados/filter/model.dart';
 import 'package:dio/dio.dart';
 
-class ApiServices {
-  static const String baseUrl = 'https://randomuser.me/api/?gender,nome,login,nat,id,dob,age';
+ApiServices() async {
+  const String baseUrl = 'https://randomuser.me/api/?results=20';
   Dio dio = Dio();
 
-  Future<List<PersonModel>> fetchUserData(int count, {String? gender, Function? onLoadingStart, Function? onLoadingEnd}) async {
-    try {
-      onLoadingStart?.call(); 
-      var queryParams = {
-        'results': count.toString(),
-        if (gender != null) 'gender': gender,
-      };
-      var response = await dio.get(baseUrl, queryParameters: queryParams);
-      if (response.statusCode == 200) {
-        List<dynamic> usersData = response.data['results'];
-        return usersData.map<PersonModel>((user) => PersonModel.fromJson(user)).toList();
-      } else {
-        throw Exception('Failed to load user data');
-      }
-    } catch (e) {
-      print('Error fetching user data: $e');
-      rethrow;
-    } finally {
-      onLoadingEnd?.call(); 
+  try {
+    var res = await dio.get(baseUrl);
+    if (res.statusCode == 200) {
+      List<dynamic> usersData = res.data['results'];
+      return usersData.map<PersonModel>((user) => PersonModel.fromJson(user)).toList();
+    } else {
+      print('Failed to load user data');
     }
+  } catch (e) {
+    print(e.toString());
   }
 }
-
--- lib/views/user_data_page_view.dart
+++ lib/views/user_data_page_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:api_dados/filter/user_detail_page.dart';
 import 'package:api_dados/filter/model.dart';
 import 'package:api_dados/filter/services.dart';
-import '../filter/gender_dropdown.dart';
 
 class UserDataPage extends StatefulWidget {
   @override
@@ -12,26 +11,21 @@ class UserDataPage extends StatefulWidget {
 class _UserDataPageState extends State<UserDataPage> {
   List<PersonModel> _filteredUsers = [];
   bool _isLoading = false;
-  String? _selectedGender;
+
+  _loadUserData() async {
+    _isLoading = true;
+    ApiServices().then((value){ 
+    setState(() {
+      _filteredUsers = value;
+      _isLoading = false;
+    });
+    });
+  }
 
   @override
   void initState() {
     super.initState();
-    _loadUserData(); // Carrega os dados iniciais ao iniciar
-  }
-
-  void _loadUserData() async {
-    setState(() => _isLoading = true);
-    try {
-      var users = await ApiServices().fetchUserData(50, gender: _selectedGender);
-      setState(() {
-        _filteredUsers = users;
-      });
-    } catch (e) {
-      print('Error fetching user data: $e');
-    } finally {
-      setState(() => _isLoading = false);
-    }
+    _loadUserData(); 
   }
 
   @override
@@ -40,59 +34,30 @@ class _UserDataPageState extends State<UserDataPage> {
       appBar: AppBar(
         title: Text('Random User Data'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 2.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: GenderDropdown(
-                    selectedGender: _selectedGender,
-                    onChanged: (newValue) {
-                      setState(() {
-                        _selectedGender = newValue;
-                      });
-                      _loadUserData();
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: _isLoading
-                ? Center(child: CircularProgressIndicator())
-                : _filteredUsers.isEmpty
-                    ? Center(child: Text('No users found'))
-                    : ListView.builder(
-                        itemCount: _filteredUsers.length,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            leading: CircleAvatar(
-                              backgroundImage: NetworkImage(_filteredUsers[index].avatarUrl),
-                              radius: 25,
-                            ),
-                            title: Text(_filteredUsers[index].name),
-                            subtitle: Text('${_filteredUsers[index].email}, ${_filteredUsers[index].city}, ${_filteredUsers[index].state}, ${_filteredUsers[index].gender}, Age: ${_filteredUsers[index].age}'),
-                            onTap: () {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) => UserDetailPage(user: _filteredUsers[index]),
-                                ),
-                              );
-                            },
-                          );
-                        },
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : _filteredUsers.isEmpty
+              ? Center(child: Text('No users found'))
+              : ListView.builder(
+                  itemCount: _filteredUsers.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(_filteredUsers[index].avatarUrl),
+                        radius: 25,
                       ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _loadUserData,
-        tooltip: 'Reload Data',
-        child: Icon(Icons.refresh),
-      ),
+                      title: Text(_filteredUsers[index].name),
+                      subtitle: Text('${_filteredUsers[index].email}, ${_filteredUsers[index].city}, ${_filteredUsers[index].state}, ${_filteredUsers[index].gender}, Age: ${_filteredUsers[index].age}'),
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => UserDetailPage(user: _filteredUsers[index]),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
     );
   }
 }
